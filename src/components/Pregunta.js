b1afae175c09ccc3fec7a274e624a7116d7c6702
Alexander Guerrero <alex.guelu@gmail.com>
2020-05-24 12:52:34 -0500

    4. Leyendo el Presupuesto del Usuario

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,23 +1,45 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Pregunta = () => {
-    return ( 
-        <Fragment>
-            <h2>Coloca tu presupuesto</h2>
-            <form>
-              <input 
-                type="number"
-                className="u-full-width"
-                placeholder="Coloca tu presupuesto. Ejm: 1500"
-              />
-              <input 
-                type="submit"
-                className="button-primary u-full-width"
-                value="Definir presupuesto"
-              />
-            </form>
-        </Fragment>
-     );
+
+  // Definir el state
+  const [cantidad, setCantidad] = useState(0);
+
+  // Función que lee el presupuesto
+  const definirPresupuesto = e => {
+    setCantidad(parseInt(e.target.value, 10));
+  }
+
+  // Submit para definir el presupuesto
+  const agregarPresupuesto = e => {
+    e.preventDefault();
+
+    // Validar...
+
+    // Si pasa la validación...
+    
+  }
+
+  return (
+    <Fragment>
+      <h2>Coloca tu presupuesto</h2>
+      <form
+        onSubmit={agregarPresupuesto}
+      >
+        <input
+          type="number"
+          className="u-full-width"
+          placeholder="Coloca tu presupuesto. Ejm: 1500"
+          onChange={definirPresupuesto}
+        />
+        <input
+          type="submit"
+          className="button-primary u-full-width"
+          value="Definir presupuesto"
+        />
+      </form>
+    </Fragment>
+  );
 }
- 
+
 export default Pregunta;
