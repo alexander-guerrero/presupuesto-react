f7ae66cdcb251fed18e38f8e2be0c21cf5c3cd52
Alexander Guerrero <alex.guelu@gmail.com>
2020-05-24 19:29:13 -0500

    9. Cargando un Componente u Otro

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   // Definir el State
   const [presupuesto, setPresupuesto] = useState(0);
   const [restante, setRestante] = useState(0);
+  const [mostrarPregunta, setMostrarPregunta] = useState(true);
 
   return (
     <div className="container">
@@ -14,18 +15,23 @@ function App() {
         <h1>Gasto Semanal</h1>
 
         <div className="contenido-principal contenido">
-          <Pregunta
-            setPresupuesto={setPresupuesto}
-            setRestante={setRestante}
-          />
-          <div className="row">
-            <div className="one-half column">
-              <Formulario />
+          {mostrarPregunta
+            ?
+            <Pregunta
+              setPresupuesto={setPresupuesto}
+              setRestante={setRestante}
+              setMostrarPregunta={setMostrarPregunta}
+            />
+            :
+            <div className="row">
+              <div className="one-half column">
+                <Formulario />
+              </div>
+              <div className="one-half column">
+                2
+              </div>
             </div>
-            <div className="one-half column">
-              2
-            </div>
-          </div>
+          }
         </div>
       </header>
     </div>
